0a31205c2a2a11edfb98d642d04c57fb5e9ff665
pierrecoetzee <pierre.coetzee@gmail.com>
2014-08-29 11:07:58 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnagramSolver::Application.config.secret_token = '4e1b560015c5767f2359a2c94a50b8a10dd2752f906bdd006d27c4346c820b3762133461b4ba9fa1c5a93bf5f4334022cb3c784b32da6521d6f3012ca1124182'
